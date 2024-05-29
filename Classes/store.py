e9f89706ad3df5fe19b26c214652b09ab3c67ee6
mac <v.kotelnikovdidi@gamil.com>
2024-05-29 21:09:02 +0300

    work DB and compare need to set classes and refactoring

--- Classes/store.py
+++ Classes/store.py
@@ -18,14 +18,15 @@ class BaseStore:
 
 
 class Item:
-    def __init__(self, name, price, article):
-        self.title = name
-        self.price = price
+    def __init__(self, primary_key, article, title, price):
+        self.id = primary_key
         self.article = article
+        self.title = title
+        self.price = price
 
     @classmethod
-    def from_tuple(cls, data_tuple):
-        return cls(*data_tuple)
+    def from_tuple(cls, db_tuple):
+        return cls(db_tuple[0], db_tuple[1], db_tuple[2], db_tuple[3])
 
 
 class Soup:
@@ -43,3 +44,6 @@ class Soup:
     def find_next_element(self, **kwargs):
         obj = self.soup.find_next(**kwargs)
         return obj
+
+
+
