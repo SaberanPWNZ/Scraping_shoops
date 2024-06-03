587bf01fe187fa0d44696a27f00c528b218978c7
mac <v.kotelnikovdidi@gamil.com>
2024-06-03 21:46:49 +0300

    working 3 shops !! and TG bot with keyboard

--- config.py
+++ config.py
@@ -1,15 +1,20 @@
 HEADERS = {
-        'Accept': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
-        'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-        #'Cache-Control': 'max-age=0',
-        #'Content-Length': "95",
-        #'Cookie': '_gcl_au=1.1.879006346.1712913771; _fbp=fb.1.1712913772878.2135253597; '
-        #           'fpestid=dD_WvU3J00umq2elgjdKYr7YAbirzlPWuYBHO2CqOX-lS1NUvJ9MEiCSJHr8Sww7w4qajQ;'
-        #           'connect.sid=s%3A3ec1bbc9-fae8-42bb-9ad5-0bb813ea7a59.hQgtlMORJ4PC7yiaCJg9wObvLqjnycVTuAk7%2'
-        #           'FvUCDVM; _gid=GA1.2.1434158348.1714118471; _ga_50K160JTP6=GS1.1.1714125994.15.1.1714126285'
-        #           '.34.0.0; _ga=GA1.2.407294421.1712913772; _gat_UA-76310630-1=1',
+    'Accept': 'application/json',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    # 'Cache-Control': 'max-age=0',
+    # 'Content-Length': "95",
+    # 'Cookie': '_gcl_au=1.1.879006346.1712913771; _fbp=fb.1.1712913772878.2135253597; '
+    #           'fpestid=dD_WvU3J00umq2elgjdKYr7YAbirzlPWuYBHO2CqOX-lS1NUvJ9MEiCSJHr8Sww7w4qajQ;'
+    #           'connect.sid=s%3A3ec1bbc9-fae8-42bb-9ad5-0bb813ea7a59.hQgtlMORJ4PC7yiaCJg9wObvLqjnycVTuAk7%2'
+    #           'FvUCDVM; _gid=GA1.2.1434158348.1714118471; _ga_50K160JTP6=GS1.1.1714125994.15.1.1714126285'
+    #           '.34.0.0; _ga=GA1.2.407294421.1712913772; _gat_UA-76310630-1=1',
 }
 
 GOOGLE_TOKEN = 'AIzaSyCgngwhjEc_86t2B2rJjFPcT-urHd3enIA'
+
+BOT_TOKEN = '7238299258:AAGlqpcSruCAbsKkflOpzYqZandlvUbz-nY'
+
+EMAIL_USERNAME = 'pricechecker868@gmail.com'
+EMAIL_PASSWORD = 'wznw ppds moyo lile'
