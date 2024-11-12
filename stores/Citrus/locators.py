054ffb40406a2abd9fd6b72b34498201d6a106f9
mac <v.kotelnikovdidi@gamil.com>
2024-11-12 20:45:41 +0200

    setup citrus _ktc

---
+++ stores/Citrus/locators.py
@@ -0,0 +1,9 @@
+class CitrusLocator:
+    WACOM_PAGE_URL = 'https://www.ctrs.com.ua/search/?page=2&query=wacom'
+    XP_PEN_PAGE_URL = 'https://www.ctrs.com.ua/search/?query=xp-pen'
+    CATALOG_GOODS = 'catalog-facet'
+    ITEM_TITLE = 'line-clamp-2 break-word MainProductCard-module__title___3fVuF'
+    ITEM_PRICE = 'medium MainProductCard-module__price___34KIa'
+    ITEM_CASHBACK = 'df aic pt8 pb8 MainProductCard-module__cashback___wq57O'
+    ITEM_STATUS = 'loop__pb'
+    PAGE_PAGINATION_CLASS = "Pagination_item__vuKKT"
