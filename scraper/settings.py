c2dfb6e61ee231cba41114f7b6e29b83840824d0
Saberan <v.kotelnikovdidi@gmail.com>
2025-03-19 20:20:28 +0200

    fix static

--- scraper/settings.py
+++ scraper/settings.py
@@ -119,11 +119,15 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
+
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    BASE_DIR / "all_static",
+    BASE_DIR / 'static',
 ]
-STATIC_ROOT = BASE_DIR / "all_static"
+
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 
 MEDIA_URL = '/media/'
